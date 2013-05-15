847ec705fb1066461ec3e5baa2e947ae020d6ea8
Chris Foster <cdbfoster@gmail.com>
2013-05-15 14:00:19 -0400

    Fixed a bug in quaternion multiplication.

--- source/math/Quaternion.hpp
+++ source/math/Quaternion.hpp
@@ -257,7 +257,7 @@ namespace Math
 	inline Quaternion Quaternion::operator*(Quaternion const &b) const
 	{
 		return Quaternion(w *  b.w - x * b.x - y * b.y - z * b.z,
-						  x *  b.w + w * b.x + y * b.z - z * b.y,
+						  w *  b.x + x * b.w + y * b.z - z * b.y,
 						  w *  b.y - x * b.z + y * b.w + z * b.x,
 						  w *  b.z + x * b.y - y * b.x + z * b.w);
 	}
